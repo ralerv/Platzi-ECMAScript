b720efe9218ea77a4782d3f57775cfda9cb50e23
ralerv <ramos_rrv@hotmail.com>
2023-04-27 12:17:52 -0500

    03 ES6: Strings

---
+++ src/ES6/03-strings.js
@@ -0,0 +1,18 @@
+let hello = "Hello";
+let world = "NONONONONO NO HAY ESO EN MIS CODIGOS";
+let epicPhrase = hello + " ME NIEGO " + world + " :(";
+let xd = "XD"
+
+
+//Template literales uwu ALT + 96
+let epicPhrase2 = `${hello} y la queso 💪🗿 ${xd}`
+console.log(epicPhrase2)
+
+//Multiline strings :0
+let lorena = "STRINGIFYFICADO xd \n" + "otra línea pero hecho con slash n";
+let lorena2 = `Esta es una línea
+y esta es otra línea ${xd}
+`;
+
+console.log(lorena);
+console.log(lorena2);
