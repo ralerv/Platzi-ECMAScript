d89db1bc473f011a2677067e266e2da995835365
ralerv <ramos_rrv@hotmail.com>
2023-04-28 10:36:34 -0500

    05 rest operator

--- src/ES6/05-rest-spread.js
+++ src/ES6/05-rest-spread.js
@@ -9,3 +9,19 @@ console.log(a,b);
 let user = {username: "o", age: 23};
 let {username,age} = user; //en llaves el mismo valor del objeto xd
 console.log(username,age)
+
+// spead operator
+let person = {name: "xd",age:2000000};
+let country = "MX";
+
+let data = { id:1,  ...person, country } //me va a devolver objet todo unido iuwu
+console.log(data)
+
+// rest
+function sum(num,...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1,1,2,3,4,5);
