2b3b773d8201c4cdb79abc9b85c2c8cd1a2cce3b
ralerv <ramos_rrv@hotmail.com>
2023-04-28 10:16:13 -0500

    05 deestructuracion

---
+++ src/ES6/05-rest-spread.js
@@ -0,0 +1,11 @@
+//expresiones de asignación de deestructuracion xd
+//arrays destructuring
+
+let fruits = ["apol","anana"];
+let [a,b] = fruits; //a es posicion 0 del array fruits
+console.log(a,b);
+
+//object destructuring
+let user = {username: "o", age: 23};
+let {username,age} = user; //en llaves el mismo valor del objeto xd
+console.log(username,age)
