145085f00a2692710a7f05071ea9d7c06768ba6b
ralerv <ramos_rrv@hotmail.com>
2023-05-03 11:29:18 -0500

    ES8: string padding y trailing commas

---
+++ src/ES8/00-object-entries.js
@@ -0,0 +1,4 @@
+const countries = {MX: "mexico", CO: "cOlombia", CL: "Chile", PE: "Perú"}
+console.log(Object.entries(countries)) // arreglo de arreglos de cada elemento
+console.log(Object.keys(countries)) //las propiedades (los primeros valores)
+console.log(Object.values(countries)) //los valores (los segundos datos xd)
---
+++ src/ES8/03-string-padding.js
@@ -0,0 +1,3 @@
+const texto = "Hello";
+console.log(texto.padStart(20,"__")) //total del texto, con que lo relleno antes del texto
+console.log(texto.padEnd(10,"-")) //total del texto, con que lo relleno después del texto
