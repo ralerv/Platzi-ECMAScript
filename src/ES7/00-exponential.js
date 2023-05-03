432bb5a5af38edb463fd3be7395858c14510de72
ralerv <ramos_rrv@hotmail.com>
2023-05-03 11:00:05 -0500

    ES7: exponentiation operator y array includes

---
+++ src/ES7/00-exponential.js
@@ -0,0 +1,3 @@
+const data = 3 ** 4 //3 a la cuatro xd
+// data **= 3 //data a la 3
+console.log(data)
---
+++ src/ES7/01-array-includes.js
@@ -0,0 +1,6 @@
+let numbers = [1,2,3,4,5,6,7,8,9];
+console.log(numbers.includes(10))
+console.log(numbers.includes(5))
+
+const list = ["xd","a","b"];
+console.log(list.includes("d")) //sensible a las mayusculas y minisculas.
