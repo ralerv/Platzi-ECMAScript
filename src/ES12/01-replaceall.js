5d08b82c88878e3115860f09ef9398a87c0c581c
ralerv <ramos_rrv@hotmail.com>
2023-05-09 10:45:40 -0500

    ES12: numeric-separators y replaceAll

---
+++ src/ES12/01-replaceall.js
@@ -0,0 +1,6 @@
+const string = "JS es uno de los lenguajes de programación de todos los tiempos"
+
+const replaceString = string.replace("JS","PHP")
+
+console.log(string)
+console.log(replaceString)
